8f350522e763fd00409e9d09343c5fac519de866
Ankur Chauhan <ankur0660341@gmail.com>
2024-07-28 17:48:46 +0530

    Some fixes

--- index.js
+++ index.js
@@ -5,5 +5,5 @@
 import {AppRegistry,LogBox} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
-LogBox.ignoreLogs(['Warning: TextInput.Icon: Support']);
+LogBox.ignoreLogs(['Warning: TextInput.Icon: Support'] );
 AppRegistry.registerComponent(appName, () => App);
